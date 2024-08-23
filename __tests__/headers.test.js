b8a45e4846a3ac94da9a671d36aa4077076050bc
Richard Addo <ramankwah03@gmail.com>
2024-08-23 20:44:40 +0200

    modified

--- __tests__/headers.test.js
+++ __tests__/headers.test.js
@@ -2,15 +2,23 @@ import '@testing-library/jest-dom';
 import { NavHeader } from '../components/header';
 import { render, fireEvent, screen } from '@testing-library/react';
 import CartProvider, { CartContext } from '../cart-provide';
+import user from "@testing-library/user-event";
+import { useContext } from 'react';
 
-// jest.mock('../libs/action', () => ({
-//     getCookies: jest.fn().mockResolvedValue({/* mock data here */ }),
-// }));
 
+
+jest.mock('../libs/action', () => ({
+    getCookies: jest.fn().mockResolvedValue({/* mock data here */ }),
+}));
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useContext: jest.fn(),
+}));
 
 describe('NavHeader', () => {
     const cartItems = [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }];
-    const setDisplayCart = jest.fn().mockResolvedValue();;
+    // const displayCart = jest.fn().mockResolvedValue(true);
     it('renders a heading', () => {
         render(<NavHeader />)
 
@@ -25,11 +33,12 @@ describe('NavHeader', () => {
 
 
 
-    // it('opens cart modal on cart icon click', () => {
-    //     const { getByAltText } = render(<NavHeader cart={cartItems} />);
-    //     fireEvent.click(getByAltText('cart'));
-    //     expect(setDisplayCart).toHaveBeenCalledWith(true);
-    // });
+    it('opens cart modal on cart icon click', async () => {
+        render(<NavHeader cart={cartItems} />);
+        const cart = screen.getByAltText('cart');
+        await user.click(cart);
+        expect(displayCart).toHaveBeenCalled();
+    });
 
     // Add more tests for other functionalities if needed
 });
